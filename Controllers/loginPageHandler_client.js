06ef4bea3d31cf8e673a260584b755f78d05a14e
Ergin <mesarikaya@gmail.com>
2017-05-08 10:55:08 +0000

    LAst commit

--- Controllers/loginPageHandler_client.js
+++ Controllers/loginPageHandler_client.js
@@ -3,18 +3,14 @@
 //Authenticated page client handler
 
 // Create a function that is enclosed and can trigger ajax function and on Ready fucnction
+
 (function () {
    //Create variables for DOM items
-   var addButton = document.querySelector('#Add_New_Poll');
-   //var deleteButton = document.querySelector('.btn-delete');
    var allPolls = document.querySelector('#All_Polls');
-   var modalButton = document.querySelector('#myBtn');
-   var myModal = document.querySelector('#myModal');
    var all_polls = document.querySelector('#all-polls');
    var view_polls = document.querySelector('#View_polls');
    var user_polls = document.querySelector('#User_polls');
    var submit_button = document.querySelector('#submitButton');
-   var New_Entry_Form = document.querySelector('#NewEntryForm');
    var user = document.querySelector('#username');
    var apiUrl = 'https://voting-poll-mesarikaya.c9users.io/loggedin';
    
@@ -26,11 +22,11 @@
 
       if (document.readyState === 'complete') {
          console.log("inside the ready function");
+         
          return fn();
       }
-      
+       all_polls.hide();
       document.addEventListener('DOMContentLoaded', fn, false);
-      
    }
 
    //Create ajax request
@@ -54,6 +50,7 @@
          var pollListHtml = "";
          var len = PollListObject.length;
          //console.log("data is: ", PollListObject);
+         
          //Create the Poll Lists for the DOM item
          for (var i = 0; i < PollListObject.length; i++) {
             var name = PollListObject[i].poll_data.Poll_name;
@@ -61,7 +58,6 @@
             if ( (typeof name !== "undefined") && (name!== null) && name !== "" 
                  && (JSON.stringify(name) !== " ") && (JSON.stringify(name) !== ""))
             {
-               //alert(JSON.stringify(name));
                pollListHtml +=  '<div class="well well-sm" >'+ '<a href="/poll/' + id + '"' +
                                    'method="get"' + 'id="' + id +'"' + 'name="' + name + '" >' +
                                    name + '</a></div>';
@@ -75,23 +71,36 @@
    }
    
    
-   // When ready, refresh the open url
-   ready(ajaxRequest('GET', window.location.href , createPollList));
+   // When ready, refresh the open url'
+   var lastFive = window.location.href.substr(window.location.href.length - 5);
+   if (lastFive === '/true' || lastFive === '/false' || lastFive === 'gedin'){
+      ready(ajaxRequest('GET', window.location.href , createPollList));
+   }
+   else{
+      ready(ajaxRequest('GET', apiUrl + '/true' , createPollList));
+   }
+   
    
    // On click, submit the new Poll to the database and update the page
    submit_button.addEventListener('click', function () {
          ajaxRequest('GET', apiUrl, createPollList);
+         
    }, false);
    
    // On click show the list of all polls
    view_polls.addEventListener('click', function () {
          ajaxRequest('GET', apiUrl + '/true', createPollList);
-         all_polls.show(); 
+         all_polls.toggle( 50, function() {
+            // Animation complete.
+          });
    }, false);
    
    // On click show the list of User's polls
    user_polls.addEventListener('click', function () {
          ajaxRequest('GET', apiUrl + '/false', createPollList);
+         all_polls.toggle( 50, function() {
+            // Animation complete.
+          });
    }, false);
    
 
